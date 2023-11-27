bd11ab0805ce02a820df73af1ea09a7253b8b84a
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2023-11-27 19:15:50 +0530

    done with writing code

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+
+export const App = () => {
+    const [count,setCount]=useState(0);
+
+    function Subtraction() {
+      setCount(count-1);
+    } 
+    function Addition() {
+      setCount(count+1);
+    }
+    function Reset(){
+      setCount(0);
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="box" >
+        <h1 className="heading">{count}</h1>
+        <div className="btnMenu">
+          <button className="sub" onClick={Subtraction} ><p className="value">SUB</p></button>
+          <button className="reset" onClick={Reset} ><p className="value">RESET</p></button>
+          <button className="add" onClick={Addition} ><p className="value">ADD</p></button>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
